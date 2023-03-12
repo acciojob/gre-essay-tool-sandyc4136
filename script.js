b1bf439ff4950391aca1988d4c472f83812a6acd
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-13 01:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //your code here
 const inputElement=document.querySelector('#evaluatedText');
-const count=document.querySelector('#wordCount');
+const h3Element=document.querySelector('#wordCount');
 
 function handleInput(e){
 	const sentence=e.target.value;
-	const countWord=sentence.split(" ").length;
-	count.innerText=countWord;
+	let match=0;
+	if(sentence.trim()===""){
+		match=0;
+	}else{
+		match = sentence.match(/\S+/ig).length;
+	}
+  h3Element.textContent = match;
 }
 inputElement.addEventListener('input',handleInput);
