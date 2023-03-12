7233dbb7049f014c7f0b354d09ab12d6c5265e01
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-13 01:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,19 +13,15 @@
 //   h3Element.textContent = match;
 // }
 // inputElement.addEventListener('input',handleInput);
-
 const textElement = document.getElementById('evaluatedText')
 const h3Element = document.getElementById('wordCount')
 
 function handelInput(e){
   const sentence = e.target.value
 	let match=0;
-	if(sentence.trim()===""){
-		match=0;
-	}else{
-		match = sentence.match(/\S+/ig).length;
-	}
-  h3Element.textContent = match;
+	if(sentence.trim()==="")  match=0;
+	else match = sentence.match(/\S+/).length;
+		h3Element.textContent = match;
 }
 textElement.addEventListener('input',handelInput)
 
