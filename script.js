7f33ccedf0122c857bca3b01cf97e9c2340e23d2
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-13 00:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const inputElement=document.querySelector('#sentence');
+const count=document.querySelector('#countDiv');
+
+function handleInput(e){
+	const sentence=e.target.value;
+	const countWord=sentence.split(" ").length;
+	count.innerText=countWord;
+}
+inputElement.addEventListener('input',handleInput);
