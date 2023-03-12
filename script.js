9c02196549df43db8961c69b48e27400452f7bea
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-13 01:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 //your code here
-const inputElement=document.querySelector('#evaluatedText');
-const h3Element=document.querySelector('#wordCount');
+// const inputElement=document.querySelector('#evaluatedText');
+// const h3Element=document.querySelector('#wordCount');
 
-function handleInput(e){
-	const sentence=e.target.value;
+// function handleInput(e){
+// 	const sentence=e.target.value;
+// 	let match=0;
+// 	if(sentence.trim()==""){
+// 		match=0;
+// 	}else{
+// 		match = sentence.match(/\S+/ig).length;
+// 	}
+//   h3Element.textContent = match;
+// }
+// inputElement.addEventListener('input',handleInput);
+
+const textElement = document.getElementById('evaluatedText')
+const h3Element = document.getElementById('wordCount')
+
+function handelInput(e){
+  const sentence = e.target.value
 	let match=0;
-	if(sentence.trim()==""){
+	if(sentence.trim()===""){
 		match=0;
 	}else{
 		match = sentence.match(/\S+/ig).length;
 	}
   h3Element.textContent = match;
 }
-inputElement.addEventListener('input',handleInput);
+textElement.addEventListener('input',handelInput)
+
+// OR
+
+
+// function countWord() {
+//       var words = document.getElementById("evaluatedText").value;
+//             var count = 0;
+//             var split = words.split(' ');
+//             for (var i = 0; i < split.length; i++) {
+//                 if (split[i] != "") {
+//                     count += 1;
+//                 }
+//             }
+// 	 document.getElementById("wordCount").innerText = count;
+// }
